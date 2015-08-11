6ec0bd891254931ecd9d9fd080e8154978374cab
runninglzw <459437440@qq.com>
2015-08-11 22:09:13 +0800

    360笔试题

---
+++ 360test/360test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _360test
+{
+    class Program
+    {
+        public int[] Getpeople(int n, int m, Dictionary<int, int> dic)
+        {
+            int[] result={0};
+            int x=0;
+            for (int i = 0; i < n; i++)
+            {
+                if(dic.ContainsKey(i))
+                    continue;
+                else
+                {
+                    int j = 0;
+                    for (; j < n; j++)
+                    {
+                        if (!dic.Contains(new KeyValuePair<int, int>(j, i)))
+                            break;
+                         
+                    }
+                    if (j < n)
+                        continue;
+                    else
+                        result[x++] = i;
+                }
+
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
